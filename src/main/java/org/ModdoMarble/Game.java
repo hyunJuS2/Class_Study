4038901e9436f7f6a032cf4ddc63901a604fdbfb
9yuhyeon <kkh2742@gmail.com>
2023-06-29 22:18:07 +0900

    add playTurn method

--- src/main/java/org/ModdoMarble/Game.java
+++ src/main/java/org/ModdoMarble/Game.java
@@ -62,8 +62,16 @@ public class Game {
     }
 
     // 플레이어가 한 턴씩 진행하도록 하는 메서드
-    public void playTurn(){
-
+    // ["규현","장미","나영","현주","정은"]
+    public Player playTurn(int playerIdx){
+        playerIdx += 1; // 현재 플레이어의 다음 순서 index
+        if (playerIdx > players.size()){
+            // 현재 플레이어 index가 모든 플레이어의 수보다 크면 처음부터
+            return players.get(0);
+        } else {
+            // 현재 플레이어의 다음 index의 플레이어를 return
+            return players.get(playerIdx);
+        }
     }
 
     // 게임의 종료 조건을 확인하는 메서드
